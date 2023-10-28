ebff594a9131a9fbec167bda94a2be9299ed3819
Daxcode55 <daxcode198@gmail.com>
2023-10-28 11:55:21 -0700

    change about content

--- script/script.js
+++ script/script.js
@@ -6,3 +6,15 @@ document.querySelector("#btn-menu").onclick = () => {
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
+
+
+const links = document.querySelectorAll(".link");
+links.forEach((getlinks) => {
+  getlinks.addEventListener("click", (e) => {
+    const getHref = getlinks.getAttribute("href");
+    const getElmHref = document.querySelector(getHref);
+    let nilaiScrol = getElmHref.offsetTop;
+    window.scrollTo(0, nilaiScrol - 70);
+    e.preventDefault();
+  })
+})
